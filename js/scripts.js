b010e70319124c3adbab1a1e4beba799177a7179
bizzclaw <bizzclaw1@gmail.com>
2017-09-29 11:31:55 -0700

    make initial constructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
+function Shop() {
+	this.toppings = [];
+}
+
+function Topping(name, price) {
+	this.name = name;
+	this.price = price;
+}
+
+Shop.prototype.AddTopping(name, price) {
+	this.toppings.push(new Topping(name, price));
+}
+
 $(document).ready(function() {
 
 });
-
