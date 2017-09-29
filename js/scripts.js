2744332ec081d4ee5e58e0fd7c915f26a3833df7
bizzclaw <bizzclaw1@gmail.com>
2017-09-29 12:34:45 -0700

    Add Pizza animations

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ function Shop(baseprice, currency) {
 }
 
 // Combine the base price with the topping price of the given pizza
-function Shop.prototype.calcPrice(pizza) {
+Shop.prototype.calcPrice = function(pizza)  {
 	return this.baseprice + this.toppings[pizza.topping].price
 }
 
-function Shop.prototype.prettyPrice(amt) {
+Shop.prototype.prettyPrice = function(amt) {
 	return parseFloat(amt * 0.01) + this.currency
 }
 
@@ -22,7 +22,7 @@ function Pizza(topping) {
 	this.topping = topping;
 }
 
-Shop.prototype.AddTopping(name, price) {
+Shop.prototype.AddTopping = function(name, price) {
 	this.toppings.push(new Topping(name, price));
 }
 
@@ -31,15 +31,36 @@ Shop.prototype.AddTopping(name, price) {
 $(document).ready(function() {
 	var shop = new Shop(699, "$");
 
-	shop.AddTopping("Salame", 0);
+	shop.AddTopping("Salame", 50);
 	shop.AddTopping("Margherita", 50);
-	shop.AddTopping("Vegitaria", 150);
-	shop.AddTopping("Hawaiian", 100);
-	shop.AddTopping("Mushroom", 100);
-	shop.AddTopping("Marinara", 100);
+	shop.AddTopping("Vegitaria", 100);
+	shop.AddTopping("Hawaiian", 150);
+	shop.AddTopping("Mushroom", 130);
+	shop.AddTopping("Marinara", 50);
 	shop.AddTopping("Mexican", 100);
-	shop.AddTopping("Quattro Formaggi", 100);
+	shop.AddTopping("Quattro Formaggi", 120);
 
+	var animtime = 250;
 
+	var pizzaAnim = anime({
+		targets: ".pizza",
+		scale: 1.2,
+		duration: animtime,
+		autoplay: false,
+		easing: "easeInQuad"
+	});
 
+	var SwapPizza = function(toppingid) {
+		var topping = shop.toppings[toppingid]
+		$(".pizza").attr("src", "img/" + topping.name + ".png")
+		pizzaAnim.play();
+		setTimeout(function(){
+			pizzaAnim.reverse();
+			pizzaAnim.play();
+		}, animtime)
+	}
+
+	SwapPizza(0);
+
+	
 });
