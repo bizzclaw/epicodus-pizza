31c121c0ee6a2b95b2035c7eb75ceb5da7b0071b
bizzclaw <bizzclaw1@gmail.com>
2017-09-29 11:42:12 -0700

    Add Pizza object and some placeholder toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-function Shop() {
+// ******* Backend ******* //
+
+function Shop(baseprice) {
 	this.toppings = [];
+	this.baseprice = baseprice;
+}
+
+function Shop.prototype.calcPrice(pizza) {
+	return this.baseprice + this.toppings[pizza.topping].price
 }
 
 function Topping(name, price) {
@@ -7,10 +14,25 @@ function Topping(name, price) {
 	this.price = price;
 }
 
+function Pizza(topping) {
+	this.topping = topping;
+}
+
+
 Shop.prototype.AddTopping(name, price) {
 	this.toppings.push(new Topping(name, price));
 }
 
+// ******* JQuery ******* //
+
 $(document).ready(function() {
+	var shop = new Shop(700);
+
+	shop.AddTopping("Cheese", 0);
+	shop.AddTopping("Peporoni", 50);
+	shop.AddTopping("Meat Lovers", 150);
+	shop.AddTopping("Pineapple", 100);
+
+
 
 });
